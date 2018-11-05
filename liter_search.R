1ddce145a95a7b5e75cb8b0799ff553f3e36dcc8
Xing Song <xsong@kumc.edu>
2018-11-05 16:10:16 -0600

    refined pubmed literature search function

---
+++ liter_search.R
@@ -0,0 +1,78 @@
+#### web scraping for AD+machine learning ####
+rm(list=ls())
+gc()
+
+# trace(utils:::unpackPkgZip, edit=TRUE)
+
+library(rentrez)
+library(rscopus)
+library(XML)
+library(RCurl)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+library(ggrepel)
+source("./util.R")
+
+AD_syn<-c("alzheimer","AD")
+EMR_syn<-c("electronic medical record","EMR",
+           "electronic health record","EHR")
+Alg_syn<-c("machine learning",
+          "learning","algorithm","analytics",
+          "big data","large data","high-dimensional data")
+
+query_grid<-expand.grid(key1=AD_syn,
+                        key2=c(EMR_syn,Alg_syn),
+                        stringsAsFactors = F) %>%
+  mutate(query_key=paste0(key1," AND ", key2))
+
+liter_rel<-list()
+
+for(i in seq_len(nrow(query_grid))){
+  query<-query_grid$query_key[i]
+  liter_evol<-c()
+  
+  start_i<-Sys.time()
+
+  # Pubmed
+  liter_pubmed<-get_pubmed_full(query,max_return=100)
+  liter_evol<-c(liter_evol,nrow(liter_pubmed))
+  
+  # Cochrane Library--TODO
+  # liter_cochrane<-get_cochrane_full(query,max_return=200)
+  # liter_evol<-c(liter_evol,nrow(liter_pubmed))
+  
+  # scopus --TODO
+  # api_key<-"311c8c86fe53aba1e2107584fc41e390"
+  # liter_scopus<-get_scopus_full(query,max_return=200)
+  # liter_evol<-c(liter_evol,nrow(liter_scopus))
+  
+  
+  
+  liter<-liter_pubmed
+  liter_evol<-c(liter_evol,nrow(liter))
+  
+  if(nrow(liter) > 0){
+    #clean-up: contain target word group I?
+    liter %<>%
+      filter(grepl(query_grid$key1[i],abstract,ignore.case = T))
+
+    #clean-up: contain target word group II?
+    liter %<>%
+      filter(grepl(query_grid$key2[i],abstract,ignore.case = T))
+  }
+  liter_evol<-c(liter_evol,nrow(liter))
+  
+  liter_rel[[query_grid$query_key[i]]]<-list(drop_out=liter_evol,
+                                             elig_liter=liter)
+
+  lapse_i<-Sys.time()-start_i
+  cat("finish searching query:'",query,"'in",lapse_i,units(lapse_i),".\n")
+  
+  Sys.sleep(15)
+}
+
+
+
--- util.R
+++ util.R
@@ -1,10 +1,16 @@
-get_pubmed_abstr<-function(query) {
+#TODO!
+keys_generator<-function(k1=list(),k2=list(),
+                         logic=c("AND","OR","BUT")){
+  
+}
+
+
+get_pubmed_abstr<-function(query,max_return=20) {
   # change spaces to + and single-quotes to URL-friendly %22 in query
   query<-gsub("'", "%22", gsub(" ", "+", query))
-  query<-paste("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=50&sort=relevance&term=", 
+  query<-paste("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=",
+               max_return,"&sort=relevance&term=", 
                query, "&usehistory=y", sep = "")
-  
-  
   
   #add an error-prevention machnism
   counter<-1 #in case there 
@@ -42,4 +48,115 @@ get_pubmed_abstr<-function(query) {
 }
 
 
+get_pubmed_full<-function(query,max_return=20) {
+  # change spaces to + and single-quotes to URL-friendly %22 in query
+  query<-gsub("'", "%22", gsub(" ", "+", query))
+  query<-paste("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=",
+               max_return,"&sort=relevance&term=", 
+               query, "&usehistory=y", sep = "")
 
+  #add an error-prevention machnism
+  counter<-1 #in case there 
+  myopt<-curlOptions(connecttimeout = 200)
+  get_url<-getURL(query,.opts=myopt)
+  temp<-try(get_url)
+  while(grepl("(Bad request)|(Error)+",temp) & counter <= 5) {
+    get_url<-getURL(query)
+    temp<-try(get_url)
+    counter<-counter+1
+  } 
+  
+  query<-xmlParse(get_url, useInternal = TRUE)
+  
+  #collect pubmed ids
+  ids<-xpathApply(query,"//IdList/Id",xmlValue)
+  ids<-unlist(ids)
+  
+  if(length(ids)==0){
+    abstracts<-list()
+  }else{
+    data_pubmed<-entrez_fetch(db = "pubmed", id = ids, rettype = "xml",
+                              parsed = TRUE)
+    #retrieve title
+    title<-xpathApply(data_pubmed, "//PubmedArticle//Article", function(x) {
+      val <- xpathSApply(x, "./ArticleTitle", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    
+    #retrieve author
+    author_ln<-xpathApply(data_pubmed, "//PubmedArticle//Article", function(x) {
+      val <- xpathSApply(x, "./AuthorList[@CompleteYN='Y']/Author[@ValidYN='Y']/LastName", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    author_fn<-xpathApply(data_pubmed, "//PubmedArticle//Article", function(x) {
+      val <- xpathSApply(x, "./AuthorList[@CompleteYN='Y']/Author[@ValidYN='Y']/ForeName", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    author<-mapply(function(x,y) paste(unlist(x),unlist(y),sep=","),author_ln,author_fn)
+    author<-sapply(author, function(x) paste(unlist(x),collapse = ";"))
+    
+    #retrieve publication date
+    pub_yr<-xpathApply(data_pubmed, "//PubmedData//History", function(x) {
+      val <- xpathSApply(x, "./PubMedPubDate[@PubStatus='pubmed']/Year", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    pub_mth<-xpathApply(data_pubmed, "//PubmedData//History", function(x) {
+      val <- xpathSApply(x, "./PubMedPubDate[@PubStatus='pubmed']/Month", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+      
+    #retrieve journal
+    journal<-xpathApply(data_pubmed, "//PubmedArticle//Article", function(x) {
+      val <- xpathSApply(x, "./Journal/Title", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    
+    #retrieve keywords
+    keywds<-xpathApply(data_pubmed, "//MedlineCitation//KeywordList", function(x) {
+      val <- xpathSApply(x, "./Keyword", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    
+    #retrieve grants
+    grands<-xpathApply(data_pubmed, "//PubmedArticle//Article", function(x) {
+      val <- xpathSApply(x, "./GrantList[@CompleteYN='Y']/Grant/GrantID", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+    grands<-lapply(grands,function(x) paste(unlist(x),collapse=","))
+    
+    #retrieve abstracts for the searched IDs
+    abstract<-xpathApply(data_pubmed, "//PubmedArticle//Article", function(x) {
+      val <- xpathSApply(x, "./Abstract", xmlValue)
+      if (length(val)==0) val <- NA_character_
+      val
+    })
+  }
+  
+  # names(abstracts)<-ids
+  tot_retn<-length(title)
+  return_df<-data.frame(retrieve_ord=seq_len(tot_retn),
+                        id=paste0("pubmed:",ids),
+                        title=unlist(title),
+                        date=unlist(paste(pub_yr,pub_mth,sep = "-")),
+                        author=unlist(author),
+                        journal=unlist(journal),
+                        grands=unlist(grands),
+                        # keywds=keywds,
+                        abstract=unlist(abstract),
+                        stringsAsFactors = F)
+  
+  return(return_df)
+}
+
+
+get_scopus_full<-function(query,max_return=20,api_key){
+  scopus_obj<-scopus_search(query,count=max_return,api_key=api_key)
+}
