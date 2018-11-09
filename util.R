7456ab16611c883d20cfd49e3f5d8e479bd9403a
sxinger <sxingerl@gmail.com>
2018-11-08 21:50:28 -0600

    updated codes

--- util.R
+++ util.R
@@ -148,37 +148,37 @@ get_pubmed_full<-function(query,max_return=20) {
              paste0("//doi.org/",doi))
     })
     
-    #retrieve citation by searching google scholar
-    title<-unlist(title)
-    cite<-c()
-    for(i in seq_along(title)){
-      #rotate agent
-      agent<-shuffle_agent()
-      #sleep
-      brk_t<-sample(20:30,1)
-      Sys.sleep(brk_t)
-      
-      query_url<-paste("https://scholar.google.com/scholar?as_q=",
-                       gsub(" ","+",title[i]),
-                       "&as_oq=&as_eq=&as_occt=any&as_sauthors=&as_publication=",
-                       "&as_ylo=&as_yhi=&hl=en&as_sdt=0%2C5",
-                       sep = "")
-      
-      get_url<-getURL(query_url,.opts=list(useragent=agent,followlocation=TRUE))
-      query<-htmlParse(get_url,encoding="UTF-8")
-      
-      cite_i<-xpathSApply(query,"//html//body//div[@class='gs_r gs_or gs_scl']//div[@class='gs_ri']//div[@class='gs_fl']//a//text()",xmlValue)
-      cite_i<-data.frame(cite_src=unlist(cite_i)[unlist(sapply(cite_i,function(x) grepl("((Cited by)|(Web of Science))+",x)))],
-                         stringsAsFactors = F) %>%
-        mutate(cite_type=str_extract(cite_src,"((Cited by)|(Web of Science))+"),
-               cite_val=as.numeric(gsub("((Cited by )|(Web of Science: ))+","",cite_src)),
-               rn=ifelse(cite_type=="Cited by",1:n(),NA)) %>%
-        fill(rn,.direction="down") %>% ungroup %>%
-        dplyr::select(rn,cite_type,cite_val) %>%
-        spread(cite_type,cite_val)
-      
-      cite %<>% bind_rows(cite_i)
-    }
+  #   #retrieve citation by searching google scholar
+  #   title<-unlist(title)
+  #   cite<-c()
+  #   for(i in seq_along(title)){
+  #     #rotate agent
+  #     agent<-shuffle_agent()
+  #     #sleep
+  #     brk_t<-sample(20:30,1)
+  #     Sys.sleep(brk_t)
+  #     
+  #     query_url<-paste("https://scholar.google.com/scholar?as_q=",
+  #                      gsub(" ","+",title[i]),
+  #                      "&as_oq=&as_eq=&as_occt=any&as_sauthors=&as_publication=",
+  #                      "&as_ylo=&as_yhi=&hl=en&as_sdt=0%2C5",
+  #                      sep = "")
+  #     
+  #     get_url<-getURL(query_url,.opts=list(useragent=agent,followlocation=TRUE))
+  #     query<-htmlParse(get_url,encoding="UTF-8")
+  #     
+  #     cite_i<-xpathSApply(query,"//html//body//div[@class='gs_r gs_or gs_scl']//div[@class='gs_ri']//div[@class='gs_fl']//a//text()",xmlValue)
+  #     cite_i<-data.frame(cite_src=unlist(cite_i)[unlist(sapply(cite_i,function(x) grepl("((Cited by)|(Web of Science))+",x)))],
+  #                        stringsAsFactors = F) %>%
+  #       mutate(cite_type=str_extract(cite_src,"((Cited by)|(Web of Science))+"),
+  #              cite_val=as.numeric(gsub("((Cited by )|(Web of Science: ))+","",cite_src)),
+  #              rn=ifelse(cite_type=="Cited by",1:n(),NA)) %>%
+  #       fill(rn,.direction="down") %>% ungroup %>%
+  #       dplyr::select(rn,cite_type,cite_val) %>%
+  #       spread(cite_type,cite_val)
+  #     
+  #     cite %<>% bind_rows(cite_i)
+  #   }
   }
   
   tot_retn<-length(title)
@@ -188,8 +188,8 @@ get_pubmed_full<-function(query,max_return=20) {
                    journal=unlist(journal),
                    date=unlist(paste(pub_yr,pub_mth,sep = "-")),
                    abstract=unlist(abstract),
-                   cite_gs=cite$`Cited by`,
-                   cite_wos=cite$`Web of Science`,
+                   # cite_gs=cite$`Cited by`,
+                   # cite_wos=cite$`Web of Science`,
                    link=link,
                    domain=domain,
                    grands=unlist(grands),
@@ -209,14 +209,19 @@ get_google_scholar_full<-function(query,max_return=20){
   keywd<-query
   #url-friendly version
   query<-strsplit(query," AND ")[[1]]
+  phr<-c()
+  wd<-c()
   for(i in seq_along(query)){
     if(str_count(trimws(query[i],"both")," ") > 0){
       phr<-gsub(" ","+",trimws(query[i],"both"))
     }else{
-      wd<-trimws(query[i])
+      wd<-c(wd,trimws(query[i]))
     }
   }
-
+  if(length(wd)>1){
+    wd<-paste0(wd,collapse = "+")
+  }
+  
   pg_n<-ceiling(max_return/10)
   data<-c()
   for(i in seq_len(pg_n)){
@@ -308,11 +313,7 @@ get_google_scholar_full<-function(query,max_return=20){
 }
 
 #TODO
-get_scopus_full<-function(query,max_return=20,api_key){
-  scopus_obj<-scopus_search(query,count=max_return,api_key=api_key)
-}
+# get_scopus_full<-function(query,max_return=20,api_key){
+#   scopus_obj<-scopus_search(query,count=max_return,api_key=api_key)
+# }
 
-
-get_wos_full<-function(query,max_return=20,api_key){
-  
-}
